3cb9cea770633bbc3df748fd5ca92bfa61762654
Brenton Bailey <brenton.bailey@openbet.com>
2026-08-17 21:11:56 +0800

    Corrected installed path

--- Formula/aether-server.rb
+++ Formula/aether-server.rb
@@ -9,16 +9,8 @@ class AetherServer < Formula
   depends_on "openjdk@21"
 
   def install
-    libexec.install "aether-server-v#{version}.jar" => "aether-server.jar"
-    
-    (libexec/"aether-server").write <<~EOS
-      #!/bin/bash
-      exec java -jar "#{libexec}/aether-server.jar" "$@"
-    EOS
-    
-    chmod 0755, libexec/"aether-server"
-
-    (bin/"aether-server").write_env_script libexec/"aether-server", Language::Java.overridable_java_home_env("21")
+    libexec.install Dir["libexec/*"]
+    (bin/"aether").write_env_script libexec/"bin/aether", Language::Java.overridable_java_home_env("21")
   end
 
   test do
